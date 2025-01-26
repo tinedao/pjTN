a2acbbe9b5329a57cbf2e4d926395a0f0ce8a25f
tinedao <tine.dao19@gmail.com>
2025-01-26 12:08:58 +0700

    add blog, add link blog navbar

--- layouts/navbar.php
+++ layouts/navbar.php
@@ -31,6 +31,7 @@ session_start();
                 <li> <a href="index.php">Home</a> </li>
                 <li> <a href="hotels.php">Hotels</a> </li>
                 <li> <a href="voucher.php">Voucher</a> </li>
+                <li> <a href="blog.php">Blog Travel</a> </li>
                 <li> <a href="owner/index.php">Owner</a> </li>
             </ul>
             <div class="hamburger">
